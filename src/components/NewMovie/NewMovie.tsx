7bc2719bc524b15723d42b7d6572601792cfc838
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2025-05-03 03:57:07 -0400

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,94 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { handleSubmit } from './index';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [ImdbUrl, setImdbUrl] = useState('');
+  const [ImdbId, setImdbId] = useState('');
+  const [count, setCount] = useState(0);
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={event =>
+        handleSubmit(
+          event,
+          title,
+          imgUrl,
+          ImdbUrl,
+          ImdbId.toString(),
+          description,
+          setTitle,
+          setDescription,
+          setImgUrl,
+          setImdbUrl,
+          setImdbId,
+          setCount,
+          onAdd,
+        )
+      }
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={value => {
+          setTitle(value);
+        }}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={value => {
+          setDescription(value);
+        }}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        required
+        onChange={value => {
+          setImgUrl(value);
+        }}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={ImdbUrl}
+        required
+        onChange={value => {
+          setImdbUrl(value);
+        }}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={ImdbId.toString()}
+        required
+        onChange={value => {
+          setImdbId(value);
+        }}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +96,12 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !title.trim() ||
+              !imgUrl.trim() ||
+              !ImdbUrl.trim() ||
+              !ImdbId.toString().trim()
+            }
           >
             Add
           </button>
--- src/components/NewMovie/index.ts
+++ src/components/NewMovie/index.ts
@@ -1,1 +1,36 @@
 export * from './NewMovie';
+import { Movie } from '../../types/Movie';
+
+export function handleSubmit(
+  event: React.FormEvent<HTMLFormElement>,
+  title: string,
+  imgUrl: string,
+  ImdbUrl: string,
+  ImdbId: string,
+  description: string,
+  setTitle: React.Dispatch<React.SetStateAction<string>>,
+  setDescription: React.Dispatch<React.SetStateAction<string>>,
+  setImdbId: React.Dispatch<React.SetStateAction<string>>,
+  setImdbUrl: React.Dispatch<React.SetStateAction<string>>,
+  setImgUrl: React.Dispatch<React.SetStateAction<string>>,
+  setCount: React.Dispatch<React.SetStateAction<number>>,
+  onAdd: (movie: Movie) => void,
+) {
+  event.preventDefault();
+
+  if (title.trim() && imgUrl.trim() && ImdbId.trim() && ImdbUrl.trim()) {
+    setTitle('');
+    setDescription('');
+    setImdbId('');
+    setImdbUrl('');
+    setImgUrl('');
+    setCount(prev => prev + 1);
+    onAdd({
+      title,
+      imgUrl,
+      imdbUrl: ImdbUrl,
+      imdbId: ImdbId,
+      description,
+    });
+  }
+}
